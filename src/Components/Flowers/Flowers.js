e688145ca498ff34b42e53fcb3f82edcf9822176
Mehadyhasanakash <mehadyhasanakash@gmail.com>
2022-03-25 23:27:28 +0600

    3rd-commmit

---
+++ src/Components/Flowers/Flowers.js
@@ -0,0 +1,14 @@
+
+
+import React from 'react';
+
+const Flowers = () => {
+    return (
+        <div className='container'>
+            <h1 className='text-success text-center mt-5'>"Life is the flower for which love is honey"</h1>
+            <h3 className='text-danger text-center mt-3'>Please Choose Flower</h3>
+        </div>
+    );
+};
+
+export default Flowers;
